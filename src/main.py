22ff260821e9b5cb3767b1becd7d42bedfc3629d
shaked4166 <shaked_s_g@walla.com>
2025-01-09 19:48:17 +0200

    add data transformer,  To do: fix a bug

--- src/main.py
+++ src/main.py
@@ -5,10 +5,12 @@ from utils.model_storage_manager import ModelStorageManager
 from utils.svm_builder import SVMBuilder
 from utils.model_analyzer import ModelAnalyzer
 from utils.model_plotter import ModelPlotter
-
+from utils.data_transformer import DataTransformer
 def main():
     """TODO: Steps..."""
     logger.info("Starting the program...")
+    data = DataTransformer("SVM","/content/drive/Shared with Me/ML Attention/Data/13AugA/condXnoTarget.mat",180).create()
+    logger.debug(data)
 
 
 if __name__ == "__main__":
